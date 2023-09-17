f16508cd48c89667e27ac8749071680ec87d733c
Roine Edmundsson <11882389+baldec@users.noreply.github.com>
2023-09-17 20:35:02 +0200

    Divine lance sequence and scribbling on checking if spell is prepared.

--- scripts/checkIfSpellIsPrepared.js
+++ scripts/checkIfSpellIsPrepared.js
@@ -9,3 +9,8 @@ let selected = canvas.tokens.controlled[0];
 console.log("", selected.actor.items.filter((item) => item.type === "spell" && item.spellcasting.isPrepared))
 
 // canvas.tokens.controlled[0].actor.items.filter((item) => item.type === "spell" && item.spellcasting.isPrepared  )
+
+
+// let selected = canvas.tokens.controlled[0];
+
+// console.log("", selected.actor.items.filter((item) => item.type === "spell" && item.spellcasting.isPrepared  ))
--- scripts/sequencer scripts/divineLance.js
+++ scripts/sequencer scripts/divineLance.js
@@ -2,21 +2,16 @@ let target = Array.from(game.user.targets)[0];
 
 new Sequence()
 .effect()
-    .file("jb2a.divine_smite.caster")
+    .file("jb2a.divine_smite.caster.blueyellow")
     .atLocation(token)
 .effect()
-    .file("jb2a.divine_smite.target")
+    .file("jb2a.energy_beam.normal.yellow.03")
+    .atLocation(token)
+    .stretchTo(target)
+    .duration(1000)
+    .delay(1500)
+.effect()
+    .file("jb2a.divine_smite.target.blueyellow")
     .atLocation(target)
-    .delay(1200)
+    .delay(1800)
 .play()
-
-
-
-// let target = Array.from(game.user.targets)[0];
-
-// new Sequence()
-// .effect()
-// .file("jb2a.energy_beam.normal.bluepink.02")
-// .atLocation(token)
-// .stretchTo(target)
-// .play()
