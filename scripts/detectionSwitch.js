dffd5899731fa8aac701931b43126b02cd161e81
ante <andreas.danielsson@gmail.com>
2023-09-18 09:36:51 +0200

    Code clean-up

--- scripts/detectionSwitch.js
+++ scripts/detectionSwitch.js
@@ -4,29 +4,29 @@ let detectionActive = game.scenes
 console.log(detectionActive);
 if (detectionActive === undefined || !detectionActive) {
   // If Undefined or false, we need to get started detecting
-  console.log("Lets go!");
+  console.log("Let's find some loot")
   game.scenes
     .filter((x) => x.active == true)[0]
     .setFlag("core", "detectionActive", true);
+
   hookId = Hooks.on("updateToken", (token, changes) => {
-    // Check if the token has the isDetecting flag set to true
+    // Check if the token has the isDetecting flag set to true, and hit has moved
     if (token.getFlag("world", "isDetecting")) {
       if (changes.x > 0 || changes.y > 0) {
-        console.log(`${token.name} is detecting and moved`);
         detectMagic(token);
-      } else {
-        console.log(`${token.name} is detecting but not moving.`);
-      }
-    } else {
-      console.log(`${token.name} is not detecting.`);
+      } 
     }
   });
+
   //Save the HookId
   game.scenes
     .filter((x) => x.active == true)[0]
     .setFlag("core", "detectionHook", hookId);
 } else {
-  // Else we are detecting, and should just stop
+
+    // Else we are detecting, and should just stop
+  console.log("Stop searching")
+
   game.scenes
     .filter((x) => x.active == true)[0]
     .setFlag("core", "detectionActive", false);
@@ -34,6 +34,7 @@ if (detectionActive === undefined || !detectionActive) {
   hookId = game.scenes
     .filter((x) => x.active == true)[0]
     .getFlag("core", "detectionHook");
+
   if (hookId) {
     Hooks.off("updateToken", hookId);
   } else {
@@ -41,21 +42,20 @@ if (detectionActive === undefined || !detectionActive) {
   }
 }
 
+
+// Find all Loot och NPC tokens, if they are within range, check their inventory to see if they got som juicy magic loot
 async function detectMagic(token) {
   let spellRange = 30;
 
   console.log("Any magic around?");
   for (const target of canvas.tokens.placeables) {
-    // Find all loot tokens, go through the Inventory in them and find all items with the magical trait. If found, calculate the distance
+    
     if (target.actor.type == "loot" || target.actor.type == "npc") {
       const distance = await canvas.grid.measureDistance(token, target);
       if (distance <= spellRange) {
         for (const inv of target.actor.inventory) {
-          for (const loot of inv.traits) {
-            if (loot == "magical") {
-              console.log("We got magic loot!");
-              let x = token.x + canvas.grid.grid.w / 2;
-              let y = token.y + canvas.grid.grid.h / 2;
+          for (const lootTrait of inv.traits) {
+            if (lootTrait == "magical") {
               await new Sequence()
                 .effect()
                     .file("jb2a.detect_magic.circle.blue")
